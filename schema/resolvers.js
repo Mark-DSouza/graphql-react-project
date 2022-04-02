b8618136b8e0a1fd5c5be661cea8cd64b1334d7e
Mark-DSouza <markdsouza434@gmail.com>
2022-04-02 22:01:56 +0530

    commit fully working graphql api

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,8 +1,56 @@
-const { UserList } = require('./../FakeData');
+const { UserList, MovieList } = require('./../FakeData');
+const lodash = require('lodash');
 
 const resolvers = {
 	Query: {
 		users: () => UserList,
+		user: (parent, args) => {
+			const { id } = args;
+			const foundUser = UserList.find((user) => user.id === Number(id));
+			return foundUser;
+		},
+		movies: () => MovieList,
+		movie: (parent, args) => {
+			const { name } = args;
+			const foundMovie = MovieList.find((movie) => (movie.name = name));
+			return foundMovie;
+		},
+	},
+
+	User: {
+		favoriteMovies: () =>
+			MovieList.filter(
+				(movie) =>
+					movie.yearOfPublication >= 2010 && movie.yearOfPublication <= 2020
+			),
+	},
+
+	Mutation: {
+		createUser: (parent, args) => {
+			const { input } = args;
+			const lastIndex = UserList.length - 1;
+			input.id = lastIndex + 1;
+			UserList.push(input);
+			return UserList[lastIndex + 1];
+		},
+
+		updateUsername: (parent, args) => {
+			const { id, username } = args.input;
+			let updatedUser;
+			UserList.forEach((user) => {
+				if (user.id === Number(id)) {
+					user.username = username;
+					updatedUser = user;
+				}
+			});
+			return updatedUser;
+		},
+
+		deleteUser: (parent, args) => {
+			const { id } = args;
+			lodash.remove(UserList, (user) => user.id === Number(id));
+			return null;
+		},
 	},
 };
 
--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -3,12 +3,52 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
 	type Query {
 		users: [User!]!
+		user(id: ID!): User
+		movies: [Movie]
+		movie(name: String!): Movie
 	}
 
 	type User {
 		id: ID!
 		name: String!
+		username: String!
 		age: Int!
+		nationality: Nationality!
+		friends: [User]
+		favoriteMovies: [Movie]
+	}
+
+	enum Nationality {
+		CANADA
+		BRAZIL
+		CHILE
+		INDIA
+		GERMANY
+	}
+
+	type Movie {
+		id: ID!
+		name: String!
+		yearOfPublication: Int!
+		isInTheaters: Boolean!
+	}
+
+	input CreateUserInput {
+		name: String!
+		username: String!
+		age: Int = 18
+		nationality: Nationality = BRAZIL
+	}
+
+	input UpdateUsernameInput {
+		id: ID!
+		username: String!
+	}
+
+	type Mutation {
+		createUser(input: CreateUserInput!): User
+		updateUsername(input: UpdateUsernameInput): User
+		deleteUser(id: ID!): User
 	}
 `;
 
